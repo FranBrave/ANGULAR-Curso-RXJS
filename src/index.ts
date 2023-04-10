96ac427217a0e91be224930f28c472ff2b411972
Fran <valiente.fco@gmail.com>
2023-04-10 19:07:48 +0200

    add exercise 1

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
+import { Observable, Observer } from 'rxjs';
 
 
-console.log('Hola Mundo!');
+const observer: Observer<any> = {
+    next: value => console.log('siguiente [next]: ', value),
+    error: error => console.warn('error [obs]: ', error),
+    complete: () => console.info('Completado [obs]')
+};
 
 
 
+// const obs$ = Observable.create(); también funcionaría
+const obs$ = new Observable<string>( subs => {
 
+    subs.next('Hola');
+    subs.next('Mundo');
 
+    subs.next('Hola');
+    subs.next('Mundo');
 
+    // Forzar un error
+    // const a = undefined;
+    // a.nombre = 'Fernando';
 
+    subs.complete()
+
+})
+
+obs$.subscribe( observer );
+
+// obs$.subscribe(
+//     valor => console.log('next:', valor),
+//     error => console.warn('error: ', error),
+//     () => console.info('Completado')
+// );
