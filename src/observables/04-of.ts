f57d4ec5602310a8bb1d16debd0fdeb052dc100a
Puesto 1 Gicoop <puesto1gicoop@Mac-mini-de-Puesto.local>
2023-07-05 09:31:37 +0200

    add fromEvent

--- src/observables/04-of.ts
+++ src/observables/04-of.ts
@@ -1,8 +1,8 @@
 import { of } from 'rxjs';
 
-const obs$ = of<number>(...[1, 2, 3, 4, 5, 6 ], 5, 6, 7);
+// const obs$ = of<number>(...[1, 2, 3, 4, 5, 6 ], 5, 6, 7);
 // const obs$ = of(1, 2, 3, 4, 5, 6);
-// const obs$ = of<any>([1, 2], {a:1, b:2}, function(){}, true, Promise.resolve(true))
+const obs$ = of<any>([1, 2], {a:1, b:2}, function(){}, true, Promise.resolve(true))
 
 
 console.log('Inicio del Obs$');
